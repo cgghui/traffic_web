d8e1ccbbc5e208f99a12a70835b1dd482cd253cd
gchen <gchen3544@gmail.com>
2020-10-17 12:12:42 +0800

    搞那么多依赖，好烦，日了狗的。

--- public/install.php
+++ public/install.php
@@ -12,10 +12,10 @@
 // [ 安装文件 ]
 // 建议安装完成后移除此文件
 // 定义应用目录
-define('APP_PATH', realpath(__DIR__ . '/../application/'));
+define('APP_PATH', __DIR__ . '/../application/');
 
 // 加载框架引导文件
-require APP_PATH . '/thinkphp/base.php';
+require __DIR__ . '/../thinkphp/base.php';
 
 // 绑定到安装控制器
 \think\Route::bind('\app\admin\command\Install', 'controller');
